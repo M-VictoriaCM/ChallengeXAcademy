791457e3e32d68a73e6b7dea4bf07a5dc8ac59a2
M-VictoriaCM <mvittoria7@gmail.com>
2024-11-10 21:30:18 -0300

    instalacion de librerias

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 //import
 const express= require('express');
+var cors = require('cors');
 const bodyParser = require('body-parser');
 const { logging } = require('./middleware')
 const { playerRouter} = require('./routes');
@@ -9,9 +10,16 @@ const { initializeDB } = require('./config/dbConfig');
 const app = express();
 const PORT =8080;
 
+
 app.use(bodyParser.json());
+app.use(cors({
+    origin: 'http://localhost:4200',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(logging);
 app.use('/player', playerRouter);
+
 
 //Run server
 (async ()=>{
